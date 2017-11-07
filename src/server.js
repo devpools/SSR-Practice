60df8f7749b02e7fe1c27c2445391beee4d67b67
ikaruce <ikaruce@gmail.com>
2017-11-08 08:03:59 +0900

    hello devpools

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './app';
+import template from './template';
+const server = express();
+
+server.use('/assets', express.static('assets'));
+
+server.get('/', (req, res) => {
+  const appString = renderToString(<App />);
+
+  res.send(template({
+    body: appString,
+    title: 'Hello DevPools from the server'
+  }));
+});
+console.log("Server listen port 8080");
+server.listen(8080);
